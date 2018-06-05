23f513c9442778386625705eb310ff61e3b546f0
Travis Jones <travisj_jones@hotmail.com>
2018-06-05 11:25:27 -0500

    created cards for each location on locations page that routes to each page for further details

--- buzzles/src/Components/LocationsPage.js
+++ buzzles/src/Components/LocationsPage.js
@@ -1,19 +1,77 @@
 import React, { Component } from 'react';
-import Storefront from './Locations/Storefront'
-import IceBarn from './Locations/IceBarn'
-import CandyShop from './Locations/CandyShop'
-import LightHouse from './Locations/Lighthouse'
+import { Card, Icon, Image } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 class Locations extends Component {
   render() {
     return (
       <div>
-        <Storefront />
-        <IceBarn />
-        <CandyShop />
-        <LightHouse />
+        <Card>
+          <Image src="https://res.cloudinary.com/travdevcloudserver/image/upload/v1528215529/storefont-inside.jpg" />
+          <Card.Content>
+            <Card.Header>Storefront</Card.Header>
+            <Card.Meta>
+              <span className="date">Established 2015</span>
+            </Card.Meta>
+            <Card.Description>Located on FM 2920</Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <Link to="/Storefront">
+              <Icon name="snowflake" />
+              Storefront Page
+            </Link>
+          </Card.Content>
+        </Card>
+        <Card>
+          <Image src="https://res.cloudinary.com/travdevcloudserver/image/upload/v1528215529/candyshop-inside.jpg" />
+          <Card.Content>
+            <Card.Header>Candy Shop</Card.Header>
+            <Card.Meta>
+              <span className="date">Established 2018</span>
+            </Card.Meta>
+            <Card.Description>Located on Spring Cypress</Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <Link to="/CandyShop">
+              <Icon name="snowflake" />
+              Candy Shop Page
+            </Link>
+          </Card.Content>
+        </Card>
+        <Card>
+          <Image src="https://res.cloudinary.com/travdevcloudserver/image/upload/v1528215530/icebarn.jpg" />
+          <Card.Content>
+            <Card.Header>Ice Barn</Card.Header>
+            <Card.Meta>
+              <span className="date">Established 2017</span>
+            </Card.Meta>
+            <Card.Description>Located on Louetta</Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <Link to="/IceBarn">
+              <Icon name="snowflake" />
+              ice Barn Page
+            </Link>
+          </Card.Content>
+        </Card>
+        <Card>
+          <Image src="https://res.cloudinary.com/travdevcloudserver/image/upload/v1528215532/lighthouse.jpg" />
+          <Card.Content>
+            <Card.Header>Storefront</Card.Header>
+            <Card.Meta>
+              <span className="date">Established 2016</span>
+            </Card.Meta>
+            <Card.Description>Located on Main St in Tomball</Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <Link to="/Lighthouse">
+              <Icon name="snowflake" />
+              Lighthouse Page
+            </Link>
+          </Card.Content>
+        </Card>
       </div>
-    )
+    );
   }
 }
 
--- buzzles/src/routes/AppRouter.js
+++ buzzles/src/routes/AppRouter.js
@@ -2,7 +2,12 @@ import React from 'react';
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import NavBar from '../Components/NavBar';
 import Header from '../Components/Header';
+
 import Locations from '../Components/LocationsPage';
+import Storefront from '../Components/Locations/Storefront';
+import CandyShop from '../Components/Locations/CandyShop';
+import Lighthouse from '../Components/Locations/Lighthouse';
+import IceBarn from '../Components/Locations/IceBarn';
 
 const AppRouter = () => {
   return (
@@ -11,6 +16,10 @@ const AppRouter = () => {
         <NavBar />
         <Route path="/" component={Header} exact={true} />
         <Route path="/Locations" component={Locations} exact={true} />
+        <Route path="/Storefront" component={Storefront} exact={true} />
+        <Route path="/CandyShop" component={CandyShop} exact={true} />
+        <Route path="/Lighthouse" component={Lighthouse} exact={true} />
+        <Route path="/IceBarn" component={IceBarn} exact={true} />
       </div>
     </BrowserRouter>
   );
